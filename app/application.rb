1935bdcbb02e1935be0840f3610761c3335ec57c
yaoyu bao <yaobao5354@gmail.com>
2020-02-18 04:26:59 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,16 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    binding.pry
+
+    if req.path.match(/items/)
+
+      item_name = req.path.split("/items/").last
+      item = @@items.find{|item| item.name == item_name}
+
+      resp.write item.price
+    end
+
+    resp.finish
   end
 
 
