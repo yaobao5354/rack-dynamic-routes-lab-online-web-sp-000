3ab301acd2fdb184200b644ec1ddcd196de2be56
yaoyu bao <yaobao5354@gmail.com>
2020-02-18 04:32:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
       item = @@items.find{|item| item.name == item_name}
       if item == nil
         resp.write "Item not found"
-        resp.status = 404
+        resp.status = 400
       else
         resp.write item.price
       end
