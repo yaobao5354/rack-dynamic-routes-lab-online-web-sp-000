1516225d925eb41881c914bc4347b196140bdf48
yaoyu bao <yaobao5354@gmail.com>
2020-02-18 04:29:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,10 @@ class Application
       item = @@items.find{|item| item.name == item_name}
 
       resp.write item.price
+    else
+      resp.write "Route not found"
+      resp.status = 404
+
     end
 
     resp.finish
